88b8a9f9b85097c89bc0d1d87ff438851affad16
MishaKozarev <mishaaaaa1986@mail.ru>
2023-01-27 23:59:38 +0300

    feat: modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -37,10 +37,41 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+  function decode(expr) {
+    let result = "";
 
-module.exports = {
-    decode
-}
+    for (let i = 0; i < expr.length; i = i + 10) {
+    //"вход" : str - все строки по 10 элементов
+    let str = "";
+    //"вход" : str1 - каждая строка из 10 элементов
+    let str1 = expr.slice(i, i + 10);
+        for (let item = 0; item < 10; item = item + 2) {
+        let str2 = "";
+    //"вход" : str2 - строка из 2 элементов каждой строки из 10 элементов
+        str2 = str1.slice(item, item + 2);
+            if (str2 === "**"){
+            str = str + " ";
+            }
+            if (str2 === "00") {
+            str = str + "";
+            }
+            if (str2 === "10") {
+            str = str + ".";
+            }
+            if (str2 === "11") {
+            str = str + "-";
+            }
+        }
+    let result0 = " "
+    let resultMorse = MORSE_TABLE[str]
+    result += resultMorse || result0;
+    }
+     return result
+
+  }
+
+  module.exports = {
+    decode,
+  };
+
+
